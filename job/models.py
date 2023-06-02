a62a2c3f36584c659bb98a7555c52d2422e28db6
EsamMegahed <esammegahed34366@gmail.com>
2023-06-02 17:32:46 +0300

    first commit

---
+++ job/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+job_Type = (
+    ("Full Time", "Full Time"),
+    ("Part Time", "Part Time"),
+)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    # location =
+    job_type = models.CharField(max_length=100, choices=job_Type)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experiences = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.title

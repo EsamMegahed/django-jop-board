25b5a95c6848cf495a4afc55bfb72515dcebdb20
EsamMegahed <esammegahed34366@gmail.com>
2023-06-06 08:46:00 +0300

    adding slug to database and use it in url feild and modul

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -32,6 +33,11 @@ class Job(models.Model):
     experiences = models.IntegerField(default=1)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to="jobs/", null=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.title
